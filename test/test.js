4077c41980811530d3a8b3e7c3e345e1394a418e
Wout Slakhorst <wout.slakhorst@nedap.com>
2020-03-17 16:10:20 +0100

    script that succeeds with latest bridge

--- test/test.js
+++ test/test.js
@@ -8,7 +8,9 @@ let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 function pauseContainer(container) {
-    var cmd = `pumba pause --duration 30s ${container}`
+    var cmd = `pumba pause --duration 10s ${container}`
+
+    // 30 and 61 seconds to test
 
     return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
@@ -25,7 +27,7 @@ function pauseContainer(container) {
 }
 
 function submitConsent(patientId) {
-    console.log("Submitting consent")
+    //console.log(`submitting consent for ${patientId}`)
 
     chai.request('http://localhost:11323')
         .post('/api/consent')
@@ -37,10 +39,10 @@ function submitConsent(patientId) {
             records: [
                 {
                     consentProof: {
-                        ID: `UUID + ${patientId}`,
+                        ID: `UUID-${patientId}`,
                         title: "someproof.pdf",
                         contentType: "application/pdf",
-                        hash: `hash + ${patientId}`
+                        hash: `hash-${patientId}`
                     },
                     period: {
                         "start": "2020-01-01T12:00:00+02:00"
@@ -51,16 +53,17 @@ function submitConsent(patientId) {
                 }
             ]
         }).then( (res) => {
-            console.log(`status ${res.status} for registering consent, body: ${JSON.stringify(res.body)}`)
+            //console.log(`status ${res.status} for registering consent, body: ${JSON.stringify(res.body)}`)
         });
 }
 
-function waitForOk(patientId) {
-    console.log("Waiting for state to propagate")
+function waitForOk(patientId, done) {
+    //console.log(`checking consent for ${patientId}`)
 
     let attemptsLeft = 10;
     const expectedValue = 'yes';
     const delayBetweenRequest = 10000;
+    var result = 'unknown'
 
     function check() {
         chai.request('http://localhost:21323')
@@ -72,21 +75,37 @@ function waitForOk(patientId) {
                 actor: "urn:oid:2.16.840.1.113883.2.4.6.1:2",
                 dataClass: "urn:oid:1.3.6.1.4.1.54851.1:MEDICAL"
             }).then( (res) => {
-                console.log(`status ${res.status} for checking consent, body: ${JSON.stringify(res.body)}`)
+                //console.log(`status ${res.status} for checking consent, body: ${JSON.stringify(res.body)}`)
 
                 if (res.body.consentGiven != expectedValue) {
                     if (attemptsLeft-- != 0) {
                         setTimeout(check, delayBetweenRequest)
                     } else {
-                        console.log("FAILURE");
+                        result = "FAILURE";
                     }
                 } else {
-                    console.log("SUCCESS");
+                    result = "SUCCESS";
                 }
             });
     }
 
     check();
+
+    var waitForHello = timeoutms => new Promise((r, j)=>{
+        var c = () => {
+            if(result != 'unknown')
+                r()
+            else if((timeoutms -= 100) < 0)
+                j('timed out!')
+            else
+                setTimeout(c, 100)
+        }
+        setTimeout(c, 100)
+    })
+    waitForHello(100000).then( () => {
+        result.should.eql('SUCCESS')
+        done()
+    });
 }
 
 describe("Node status", () => {
@@ -100,22 +119,26 @@ describe("Node status", () => {
     });
 });
 
+function containerTest(container, patientId, done) {
+    pauseContainer(container);
+
+    // submit consent request
+    submitConsent(patientId);
+
+    // poll otherside
+    waitForOk(patientId, done);
+}
+
 describe("Record consent", () => {
-    var currentPatient = 4;
+    var currentPatient = 0;
     var containers = ["timonb", "timonc", "pumba", "pumbab", "pumbac", "notary"];
 
-    //containers.forEach( (item) => {
+    containers.forEach( (item) => {
         // pause container
-    it("transfers consent to the other side", () => {
-        pauseContainer("timonc")
-
-        // submit consent request
-        submitConsent(currentPatient);
-
-        // poll otherside
-        waitForOk(currentPatient);
-
-        // next patient
-        currentPatient++;
+        describe("transfers consent to the other side when pausing " + item, () => {
+            it("works" + item, (done) => {
+                containerTest(item, currentPatient++, done);
+            }).timeout(120000);
+        });
     });
 });
